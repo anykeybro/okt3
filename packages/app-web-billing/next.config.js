c7b2c3c732e3954b639b354e168022fe593501d0
Maksim <mjs-studio@inbox.ru>
2025-09-25 22:14:14 +0300

    поправил конфигурации nginx

--- packages/app-web-billing/next.config.js
+++ packages/app-web-billing/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/billing', // Указывает базовый путь для маршрутизации
+  assetPrefix: '/billing', // Указывает базовый путь для загрузки статических ресурсов (JS, CSS)
   transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material'],
 }
 
--- packages/app-web/next.config.js
+++ packages/app-web/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // basePath: '', // Указывает базовый путь для маршрутизации
+  // assetPrefix: '', // Указывает базовый путь для загрузки статических ресурсов (JS, CSS)
   experimental: {
     appDir: true,
   },
